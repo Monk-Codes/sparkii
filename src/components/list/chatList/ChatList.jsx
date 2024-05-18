070fff63b12d22e2145bd89717842aa71a45966d
monk-codes <your_donotreply_email_id>
2024-05-18 15:56:09 +0530

    search icon

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,12 +1,12 @@
 const ChatList = () => {
  return (
   <div className="chatlist">
-   <div className="search">
-    <div className="searchbar">
-     <img src="./search.png" alt="search" />
-     <input type="text" placeholder="search" />
+   <div className="search flex items-center gap-5 p-4">
+    <div className="searchbar flex-1 bg-slate-800 flex items-center gap-5 rounded-xl p-3">
+     <img src="./search.png" alt="search" className="h-4 w-4" />
+     <input type="text" placeholder="search" className="bg-transparent rounded-none outline-none text-white flex-1" />
     </div>
-    <img src="./plus.png" alt="plus" />
+    <img src="./plus.png" alt="plus" className="h-8 w-8 bg-slate-800 p-1 cursor-pointer rounded-xl" />
    </div>
   </div>
  );
